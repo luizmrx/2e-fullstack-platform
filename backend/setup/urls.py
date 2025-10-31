97d5731f8179d40c3901c46aa1b5c0c5d0950b2c
Luiz Souza <luizmrx91@gmail.com>
2025-10-31 12:11:18 -0300

    feat: versão final do site

--- backend/setup/urls.py
+++ backend/setup/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('area-restrita/', admin.site.urls),
     path('api/', include('api.urls')),
     path('blog/', include('blog.urls')),
     # re_path(r'^.*$', IndexView.as_view(), name='index'),
